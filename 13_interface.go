01609d774cd24424f4d52e9c4aa4e18fd49bd2c4
Mehdi <mehditeymorian322@gmail.com>
2020-09-18 23:22:45 +0430

    Interface

---
+++ 13_interface.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Define interface
+// use interface keyword
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+// ==========================
+// Define functions
+func (c Circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
+func main() {
+	cirle := Circle{x: 0, y: 0, radius: 5}
+	rectangle := Rectangle{width: 10, height: 10}
+
+	fmt.Printf("Circle Area: %f\n", getArea(cirle))
+	fmt.Printf("Rectangle Area: %f\n", getArea(rectangle))
+
+}
